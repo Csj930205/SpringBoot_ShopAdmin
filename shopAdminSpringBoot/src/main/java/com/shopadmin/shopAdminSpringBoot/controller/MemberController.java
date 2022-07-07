81b4b44aaad29e477dde68c094556ff58ef88455
Sungjun <tjdwns132@naver.com>
2022-07-07 11:53:43 +0900

    회원관리 수정 작업중

--- shopAdminSpringBoot/src/main/java/com/shopadmin/shopAdminSpringBoot/controller/MemberController.java
+++ shopAdminSpringBoot/src/main/java/com/shopadmin/shopAdminSpringBoot/controller/MemberController.java
@@ -1,9 +1,14 @@
 package com.shopadmin.shopAdminSpringBoot.controller;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.shopadmin.shopAdminSpringBoot.repository.MemberRepository;
@@ -19,5 +24,11 @@ public class MemberController {
 		Iterable<MemberVo> memberList=mr.findAllByOrderBySignupTimeDesc();
 		model.addAttribute("memberList",memberList);
 		return "/member/memberlist";
+	}
+	@GetMapping("/admin/membermodify/{id}")
+	public String update(@PathVariable("id") String id, Model model) {
+		Optional<MemberVo> memVo=mr.findById(id);
+		model.addAttribute("memVo",memVo);
+		return "/member/membermodify";
 	}
 }
